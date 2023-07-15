0565bc591e87598cdc0a80bf0b7927d03f327222
Ankit <114281117+ankitgupta143@users.noreply.github.com>
2023-07-15 22:37:20 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <>
+       {/* <CssBaseline /> */}
+      <Header/>
+    </>
+  );
+}
+
+export default App;
